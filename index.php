afbf4e7c375a0e4b403b295524cbecca6b3f2a91
Shannnnn <sacanoy16@gmail.com>
2020-01-24 12:25:16 +0800

    initial

--- index.php
+++ index.php
@@ -3,23 +3,20 @@
 	<head>
       <script src = "https://maps.googleapis.com/maps/api/js"></script>
    </head>
-
-	<body>
-
-		<h1>Google Map</h1>
-
-		<div id="googleMap" style="width:100%;height:400px;"></div>
-
 		<script>
 			function myMap() {
 				var mapProp= {
 	 			 center:new google.maps.LatLng(51.508742,-0.120850),
 	 			 zoom:5,
+	 			 mapTypeId:google.maps.MapTypeId.ROADMAP
 				};
 			
 				var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 			}
 		</script>
+	<body>
+		<h1>Google Map</h1>
 
+		<div id="googleMap" style="width:100%;height:400px;"></div>
 	</body>
 </html>
